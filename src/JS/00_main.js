40f6cf9dd33d894b070fdd76f8dd682525b5540a
BarbaraGB1 <bargarbo@gmail.com>
2022-11-13 22:14:39 +0100

    function click search

--- src/JS/00_main.js
+++ src/JS/00_main.js
@@ -1,8 +1,11 @@
 "use strict"
 const personajesList = document.querySelector(".js-charac")
-
+const btnSearch = document.querySelector(".js-btnSearch")
+const input = document.querySelector(".js-inputSearch")
 
 let character =[]
+
+//Petición al servidor para obtener lista de personajes
 fetch('https://breakingbadapi.com/api/characters')
  .then((response) => response.json())
  .then((charactersList) => {
@@ -12,13 +15,24 @@ fetch('https://breakingbadapi.com/api/characters')
     PaintCharacter ()
 })
 
+
+//Función para que pinte personajes según los reciba
 function PaintCharacter (){
     let html ='';
 
     for(const actor of character){
-        html+=`<li class="listRender"><article><div><img class="imgActor" src="${actor.img}"></div><h2>${actor.name}</h2><h3>${actor.status}</h3></article></li>`;
+        html+=`<li class="listRender"><article><div><img class="imgActor" src="${actor.img}" alt="Imagen actor/actriz"></div><h2>${actor.name}</h2><h3>${actor.status}</h3></article></li>`;
     }
 
     personajesList.innerHTML= html;
 
 }
+
+function handlerClick(){
+    const inputValue = input.value.toLowerCase()
+    const filterList = character.filter((character) =>character.name.toLowerCase().includes(inputValue) )
+    console.log(filterList)
+    console.log("hola")
+}
+
+btnSearch.addEventListener("click",handlerClick)
