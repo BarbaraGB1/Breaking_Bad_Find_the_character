8bdbdc3d2419e1fb26e35644e7250fe548e820bd
BarbaraGB1 <bargarbo@gmail.com>
2022-11-15 14:14:00 +0100

    fix error

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,33 +1,30 @@
-'use strict';
-
-const personajesList = document.querySelector('.js-charac');
-const btnSearch = document.querySelector('.js-btnSearch');
-const input = document.querySelector('.js-inputSearch');
-const favouritesList = document.querySelector('.js-select');
-const spoiler = document.querySelector('.js-modal');
-const btnClose = document.querySelector('.js-btnClose');
-const blurDiv = document.querySelector('.js-blur');
-const reset = document.querySelector('.js-reset');
+"use strict";
+const personajesList = document.querySelector(".js-charac");
+const btnSearch = document.querySelector(".js-btnSearch");
+const input = document.querySelector(".js-inputSearch");
+const favouritesList = document.querySelector(".js-select");
+const spoiler = document.querySelector(".js-modal");
+const btnClose = document.querySelector(".js-btnClose");
+const reset = document.querySelector(".js-reset");
+const sectionFav = document.querySelector(".js-sectionFav ");
 
 let character = [];
 let favourites = [];
+
 //Función agregarle a cada artículo de personajes el evento de click
 function listenCurrent() {
-  const allCharacter = document.querySelectorAll('.js-clickCharacter');
-
+  const allCharacter = document.querySelectorAll(".js-clickCharacter");
 
   for (const oneArticle of allCharacter) {
-    oneArticle.addEventListener('click', handleClickFav);
+    oneArticle.addEventListener("click", handleClickFav);
   }
 }
 
 //función detectar el click con el id del personaje y decirle que si no pertenece a favoritos lo meta y si no lo saque
 
 function handleClickFav(event) {
-
-
-  event.currentTarget.classList.toggle('selected');
-
+  event.currentTarget.classList.toggle("selected");
+  sectionFav.classList.remove("hidden");
 
   const selectedCha = character.find(
     (eachCha) => eachCha.char_id === parseInt(event.currentTarget.id)
@@ -42,21 +39,19 @@ function handleClickFav(event) {
     favourites.splice(favouriteSelected, 1);
   }
 
-
-  localStorage.setItem('favourites', JSON.stringify(favourites));
+  localStorage.setItem("favourites", JSON.stringify(favourites));
   renderFavourites();
 }
 //función para que pintar los favoritos
 
-
 function renderFavourites() {
-  let html = '';
+  let html = "";
 
   for (const fav of favourites) {
     html += paintFavourites(fav);
   }
   favouritesList.innerHTML = html;
-  listenClickRemove()
+  listenClickRemove();
 }
 
 function paintFavourites(listFav) {
@@ -65,23 +60,22 @@ function paintFavourites(listFav) {
 }
 //Función para que pinte personajes según los reciba
 function paintCharacter(listCharacter) {
-
   const favouriteSelected = favourites.findIndex(
-    (eachCha) => eachCha.char_id === parseInt(listCharacter.char_id));
-  let classFav = '';
+    (eachCha) => eachCha.char_id === parseInt(listCharacter.char_id)
+  );
+  let classFav = "";
   if (favouriteSelected === -1) {
-    classFav = '';
+    classFav = "";
   } else {
-    classFav = 'selected';
+    classFav = "selected";
   }
-
 
   let html = `<li class="listRender"><article id="${listCharacter.char_id}" class= "article ${classFav} js-clickCharacter"><img class="imgActor" src="${listCharacter.img}" alt="Imagen actor/actriz"><h2 class="nameCha">${listCharacter.name}</h2><h3 class= "status">${listCharacter.status}</h3></article></li>`;
   return html;
 }
 
 function renderSearch(searchList) {
-  let html = '';
+  let html = "";
   for (let i = 0; i < searchList.length; i++) {
     html += paintCharacter(searchList[i]);
   }
@@ -100,27 +94,27 @@ function handlerClick(event) {
   renderSearch(filterList);
 }
 //función quitar uno lista fav
-function listenClickRemove(){
-  const btnRemove = document.querySelectorAll('.js-removeFav');
-  for(const eachBtn of btnRemove){
-    eachBtn.addEventListener('click',handleClickRemove);
+function listenClickRemove() {
+  const btnRemove = document.querySelectorAll(".js-removeFav");
+  for (const eachBtn of btnRemove) {
+    eachBtn.addEventListener("click", handleClickRemove);
   }
 }
 
-function handleClickRemove(event){
+function handleClickRemove(event) {
   const posOneFav = favourites.findIndex(
     (selectFav) => selectFav.char_id === parseInt(event.currentTarget.id)
   );
-console.log(event.currentTarget.id);
-  if (posOneFav !== -1)
-     {
+  console.log(event.currentTarget.id);
+  if (posOneFav !== -1) {
     favourites.splice(posOneFav, 1);
   }
-  localStorage.setItem('favourites', JSON.stringify(favourites));
+  localStorage.setItem("favourites", JSON.stringify(favourites));
   renderFavourites();
+  renderSearch(character);
 }
 //Petición al servidor para obtener lista de personajes
-fetch('https://breakingbadapi.com/api/characters')
+fetch("https://breakingbadapi.com/api/characters")
   .then((response) => response.json())
   .then((charactersList) => {
     character = charactersList;
@@ -128,46 +122,38 @@ fetch('https://breakingbadapi.com/api/characters')
     renderSearch(character);
   });
 //función limpiar fav
-function handleReset(event){
-  debugger;
+function handleReset(event) {
   event.preventDefault();
   let html = "";
-  localStorage.removeItem('favourites');
-  favourites=[];
+  localStorage.removeItem("favourites");
+  favourites = [];
   favouritesList.innerHTML = html;
   renderSearch(character);
- 
+  sectionFav.classList.add("hidden");
 }
 //evento click botón de buscar
-btnSearch.addEventListener('click', handlerClick);
+btnSearch.addEventListener("click", handlerClick);
 
 //evento botón resetear fav
-reset.addEventListener("click",handleReset);
+reset.addEventListener("click", handleReset);
 
 //pintar lo que hay guardado en localStorage
-const savedFavourites = JSON.parse(localStorage.getItem('favourites'));
-
+const savedFavourites = JSON.parse(localStorage.getItem("favourites"));
 
 if (savedFavourites !== null) {
   favourites = savedFavourites;
   renderFavourites();
 }
 
-
-
-
 //Funciones para el modal.Add/Remove
 
 function showModal() {
-  spoiler.classList.remove('hidden');
-  blurDiv.classList.remove('hidden');
+  spoiler.classList.remove("hidden");
 }
 
 setTimeout(showModal, 2000);
 
-
 function handleClose() {
-  spoiler.classList.add('hidden');
-  blurDiv.classList.add('hidden');
+  spoiler.classList.add("hidden");
 }
-btnClose.addEventListener('click', handleClose);
+btnClose.addEventListener("click", handleClose);
